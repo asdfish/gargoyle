f81b8edbf413754a04a78a5af00f527c10fc475e
asdfish <lgray3420@gmail.com>
2025-07-27 10:33:57 -0400

    Revert "use `bool` instead of `_Bool`"
    This reverts commit 0c8560e475567200fff3e923abd42283464257bb.

--- src/reexports.c
+++ src/reexports.c
@@ -26,7 +26,7 @@ const SCM GARGOYLE_REEXPORTS_SCM_BOOL_T = SCM_BOOL_T;
 const SCM GARGOYLE_REEXPORTS_SCM_BOOL_F = SCM_BOOL_F;
 const SCM GARGOYLE_REEXPORTS_SCM_UNDEFINED = SCM_UNDEFINED;
 
-bool gargoyle_reexports_scm_is_true(SCM b) {
+_Bool gargoyle_reexports_scm_is_true(SCM b) {
   return scm_is_true(b);
 }
 
