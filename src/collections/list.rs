6683e81445b65009e73182230a4cc870292cce36
asdfish <lgray3420@gmail.com>
2025-08-01 19:37:42 -0400

    add lists

--- src/collections/list.rs
+++ src/collections/list.rs
@@ -20,12 +20,15 @@
 
 use {
     crate::{
+        Guile,
         reference::ReprScm,
         scm::{Scm, ToScm},
+        sys::{SCM_EOL, scm_cons},
     },
     std::marker::PhantomData,
 };
 
+#[derive(Debug)]
 #[repr(transparent)]
 pub struct List<'gm, T>
 where
@@ -35,14 +38,43 @@ where
     _marker: PhantomData<T>,
 }
 unsafe impl<'gm, T> ReprScm for List<'gm, T> where T: ToScm<'gm> {}
-// impl<'gm, T> FromIterator<T> for List<'gm, T>
-// where
-//     T: ToScm<'gm>,
-// {
-//     fn from_iter<I>(iter: I) -> Self
-//     where
-//         I: IntoIterator<Item = T>,
-//     {
-//         todo!()
-//     }
-// }
+impl<'gm, T> List<'gm, T>
+where
+    T: ToScm<'gm>,
+{
+    pub fn new<I>(iter: I, guile: &'gm Guile) -> Self
+    where
+        I: IntoIterator<Item = T>,
+    {
+        Self {
+            scm: Scm::from_ptr(
+                iter.into_iter()
+                    .fold(unsafe { SCM_EOL }, |cdr, car| unsafe {
+                        scm_cons(car.to_scm(guile).as_ptr(), cdr)
+                    }),
+                guile,
+            ),
+            _marker: PhantomData,
+        }
+    }
+}
+impl<T> PartialEq for List<'_, T>
+where
+    T: for<'a> ToScm<'a>,
+{
+    fn eq(&self, r: &Self) -> bool {
+        self.scm.is_equal(&r.scm)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use {super::*, crate::with_guile};
+
+    #[cfg_attr(miri, ignore)]
+    #[test]
+    fn list_construction() {
+        with_guile(|guile| assert_eq!(List::new([1, 2, 3], guile), List::new([1, 2, 3], guile)))
+            .unwrap();
+    }
+}
